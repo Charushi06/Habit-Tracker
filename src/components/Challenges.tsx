03c9e21bf3961589f62ac7c57f79007c509320fa
Tanishka Chopadkar <chopadkartanishka@gmail.com>
2026-05-19 20:51:22 +0530

    Add confirmation dialog before deleting challenges

--- src/components/Challenges.tsx
+++ src/components/Challenges.tsx
@@ -292,7 +292,15 @@ export function Challenges() {
                       </div>
                     </div>
                     <button
-                      onClick={() => deleteChallenge(challenge.id)}
+                    onClick={() => {
+  if (
+    window.confirm(
+      "Are you sure you want to delete this challenge? Your progress will be lost."
+    )
+  ) {
+    deleteChallenge(challenge.id);
+  }
+}}
                       className="p-2 text-gray-400 hover:text-red-600 dark:hover:text-red-400 rounded-lg transition-colors"
                     >
                       <Trash2 className="w-4 h-4" />
@@ -367,7 +375,15 @@ export function Challenges() {
                       </div>
                     </div>
                     <button
-                      onClick={() => deleteChallenge(challenge.id)}
+                     onClick={() => {
+  if (
+    window.confirm(
+      "Are you sure you want to delete this challenge? Your progress will be lost."
+    )
+  ) {
+    deleteChallenge(challenge.id);
+  }
+}}
                       className="p-2 text-gray-400 hover:text-red-600 dark:hover:text-red-400 rounded-lg transition-colors"
                     >
                       <Trash2 className="w-4 h-4" />
